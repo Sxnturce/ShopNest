ff911525ac40e075f11cad5c1db4a79a236b9a7f
SebastianSR <102806578+Sxnturce@users.noreply.github.com>
2024-10-15 18:53:59 -0500

    add localstorage from products

--- client/src/views/Home.tsx
+++ client/src/views/Home.tsx
@@ -4,8 +4,16 @@ import { clientAxios } from "../helpers/AxiosClient";
 import Card from "../components/Card";
 
 export default function Home() {
-	const [products, setProducts] = useState<Product[]>([]);
-	const [load, setLoad] = useState(false);
+	function saved() {
+		let arr: Product[] = [];
+		const query = localStorage.getItem("products-nest");
+		if (query) {
+			arr = JSON.parse(query);
+		}
+		return arr;
+	}
+
+	const [products, setProducts] = useState<Product[]>(saved);
 
 	useEffect(() => {
 		async function readData() {
@@ -14,15 +22,18 @@ export default function Home() {
 				setProducts(result.data);
 			} catch (e) {
 				console.log(e);
-			} finally {
-				setLoad(true);
 			}
 		}
 		readData();
 	}, []);
+
+	useEffect(() => {
+		localStorage.setItem("products-nest", JSON.stringify(products));
+	}, [products]);
+
 	return (
 		<>
-			{!load ? (
+			{products.length === 0 ? (
 				<span className="loader w-5 h-5 block mt-10"></span>
 			) : (
 				<section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5">
